67f8f725750e3309f78835deaeb4c9cfc1504df8
utku <uuacikalin@gmail.com>
2017-01-28 21:02:15 +0300

    Demo files

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,21 @@
 package com.mycompany.app;
 
-import java.util.*;
-import java.lang.Math;
+import static spark.Spark.*;
 
-public class App 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+public class App
 {
     public static List<Integer> findCommonElements(List<Integer> array1, List<Integer> array2, List<Integer> array3) {
 
-    	if(array1 == null || array2 == null || array3 == null)
-    		return null;
+        if(array1 == null || array2 == null || array3 == null)
+            return null;
 
         HashMap<Integer,Integer> map1 = new HashMap<>();
         HashMap<Integer,Integer> map2 = new HashMap<>();
@@ -40,4 +47,66 @@ public class App
 
         return commons;
     }
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+
+        post("/compute", (req,res) -> {
+
+
+            String input1 = req.queryParams("input1");
+            java.util.Scanner scanner = new java.util.Scanner(input1);
+            scanner.useDelimiter("[;\r\n]+");
+            ArrayList<Integer> list1 = new ArrayList<>();
+            while(scanner.hasNext()) {
+                int value = Integer.parseInt(scanner.next().replaceAll("\\s",""));
+                list1.add(value);
+            }
+            System.out.println(list1);
+
+            String input2 = req.queryParams("input2");
+            scanner = new java.util.Scanner(input2);
+            scanner.useDelimiter("[;\r\n]+");
+            ArrayList<Integer> list2 = new ArrayList<>();
+            while(scanner.hasNext()) {
+                int value = Integer.parseInt(scanner.next().replaceAll("\\s",""));
+                list2.add(value);
+            }
+            System.out.println(list2);
+
+            String input3 = req.queryParams("input3");
+            scanner = new java.util.Scanner(input3);
+            scanner.useDelimiter("[;\r\n]+");
+            ArrayList<Integer> list3 = new ArrayList<>();
+            while(scanner.hasNext()) {
+                int value = Integer.parseInt(scanner.next().replaceAll("\\s",""));
+                list3.add(value);
+            }
+            System.out.println(list3);
+
+            ArrayList<Integer> result = (ArrayList<Integer>) findCommonElements(list1, list2, list3);
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+        get("/compute", (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+     }
 }
